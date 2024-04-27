77da69803df092dea6d15e8472486cc0b2f8db92
Happy Felix Chukwuma <happychukwuma@gmail.com>
2024-04-27 05:06:21 -0700

    task

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,20 +2,29 @@
 """
 This module contains the principal application
 """
-
 from models import storage
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from os import getenv
+from flask_cors import CORS
 
 app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
 def close_db(obj):
     """ calls methods close() """
     storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """ Loads a custom 404 page not found """
+    return make_response(jsonify({"error": "Not found"}), 404)
+
 
 if __name__ == "__main__":
 
